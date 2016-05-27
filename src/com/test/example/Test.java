16be94cd9b0aaec3863b50a18de24c33f727d68a
su1107 <535355594@qq.com>
2016-05-27 15:47:09 +0800

    guodai third time

--- src/com/test/example/Test.java
+++ src/com/test/example/Test.java
@@ -6,6 +6,8 @@ public class Test {
 public static void main(String[] args) {
 	System.out.println("第一次编辑");
 	System.out.println("第二次编辑");
+	System.out.println("guodai third time to commit!");
+	
 	
 }
 
